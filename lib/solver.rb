1c8c7ec14050422e31d696503a200c0b98c4183f
Abdurahim .M <Abdugreat2@gmail.com>
2023-06-30 23:18:37 +0300

    Fix linters

--- lib/solver.rb
+++ lib/solver.rb
@@ -1,24 +1,25 @@
 class Solver
-  def factorial num
-    if (num == 0)
+  def factorial(num)
+    if num.zero?
       return 1
-    elsif (num<0)
-      raise ArgumentError.new("Expected positive number, got #{num}")
+    elsif num.negative?
+      raise ArgumentError, "Expected positive number, got #{num}"
     end
-    num *= factorial(num-1)
+
+    num * factorial(num - 1)
   end
 
-  def reverse word
+  def reverse(word)
     word.reverse
   end
 
-  def fizzbuzz num
-    if num%3 == 0 and num%5 ==0
-      "fizzbuzz"
-    elsif num%3 == 0
-      "fizz"
-    elsif num%5 ==0
-      "buzz"
+  def fizzbuzz(num)
+    if (num % 3).zero? and (num % 5).zero?
+      'fizzbuzz'
+    elsif (num % 3).zero?
+      'fizz'
+    elsif (num % 5).zero?
+      'buzz'
     else
       num.to_s
     end
